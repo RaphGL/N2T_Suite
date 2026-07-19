8c431c45db2a665c00fcbd930b70f8fbcd8e69ae
RaphGL <raphfl.dev@gmail.com>
2026-07-19 13:53:51 -0100

    started working on bitmap operations

--- src/gui/bitmap.cpp
+++ src/gui/bitmap.cpp
@@ -10,60 +10,158 @@ namespace gui::bitmap {
 ViewCtx::ViewCtx(gui::Context *ctx)
     : _ctx { ctx } { }
 
-void ViewCtx::show() {
-   constexpr std::uint16_t default_height = 16, default_width = 16;
-   constexpr std::uint16_t max_height = 256, max_width = 512;
+std::string_view ViewCtx::view_name() const { return "Bitmap Editor"; }
 
-   static std::uint16_t pixel_height = default_height, pixel_width = default_width;
-   static std::array<bool, max_width * max_height> pixels { };
+bool &ViewCtx::bitmap_at(std::uint16_t x, std::uint16_t y) {
+   if (x > _pixels_width) {
+      throw "X index is bigger than the current bitmap width";
+   }
+   if (y > _pixels_height) {
+      throw "Y index is bigger than the current bitmap height";
+   }
+   return _pixels.at(y * _pixels_width + x);
+}
+
+void ViewCtx::bitmap_invert() {
+   for (std::size_t i = 0; i < _pixels_width * _pixels_height; i++) {
+      _pixels.at(i) = !_pixels.at(i);
+   }
+}
+
+void ViewCtx::bitmap_shift(ShiftDirection dir) {
+   switch (dir) {
+   case ShiftDirection::Left:
+      // TODO: not properly handling shifts when left edge is reached
+      for (std::uint16_t y = 0; y < _pixels_height; y++) {
+         for (std::uint16_t x = 0; x < _pixels_width; x++) {
+            auto &curr = bitmap_at(x, y);
+            bool next = false;
+            if (x != _pixels_width - 1) {
+               next = bitmap_at(x + 1, y);
+            }
+            curr = next;
+         }
+      }
+      break;
+
+   case ShiftDirection::Right:
+      for (std::uint16_t y = 0; y < _pixels_height; y++) {
+         for (std::uint16_t x = _pixels_width - 1; x > 0; x--) {
+            auto &curr = bitmap_at(x, y);
+            bool prev = false;
+            if (x != 0) {
+               prev = bitmap_at(x - 1, y);
+            }
+            curr = prev;
+         }
+      }
+      break;
+
+   case ShiftDirection::Up:
+      // TODO: not properly handling shifts when upper edge is reached
+      for (std::uint16_t y = 0; y < _pixels_height; y++) {
+         for (std::uint16_t x = 0; x < _pixels_width; x++) {
+            auto &curr = bitmap_at(x, y);
+            bool next = false;
+            if (y != _pixels_height - 1) {
+               next = bitmap_at(x, y + 1);
+            }
+            curr = next;
+         }
+      }
+      break;
+
+   case ShiftDirection::Down:
+      for (std::uint16_t y = _pixels_height - 1; y > 0; y--) {
+         for (std::uint16_t x = 0; x < _pixels_width; x++) {
+            auto &curr = bitmap_at(x, y);
+            bool next = false;
+            if (y != 0) {
+               next = bitmap_at(x, y - 1);
+            }
+            curr = next;
+         }
+      }
+      break;
+   }
+}
+
+void ViewCtx::show() {
+   constexpr std::size_t button_size = 15;
 
    /* Bitmap controls */ {
       ImGui::BeginGroup();
 
       ImGui::SetNextItemWidth(100);
-      ImGui::InputScalarN("##pixel-height", ImGuiDataType_U16, &pixel_height, 1);
+      ImGui::InputScalarN("##pixel-height", ImGuiDataType_U16, &_pixels_height, 1);
       ImGui::SameLine();
       ImGui::TextUnformatted("x");
 
       ImGui::SameLine();
       ImGui::SetNextItemWidth(100);
-      ImGui::InputScalarN("##pixel-width", ImGuiDataType_U16, &pixel_width, 1);
+      ImGui::InputScalarN("##pixel-width", ImGuiDataType_U16, &_pixels_width, 1);
 
       ImGui::SameLine();
       if (ImGui::Button("Reset")) {
-         pixel_width = default_width;
-         pixel_height = default_height;
+         _pixels_width = default_width;
+         _pixels_height = default_height;
       }
 
       ImGui::SameLine();
       if (ImGui::Button("Clear")) {
-         std::fill(pixels.begin(), pixels.end(), false);
+         std::fill(_pixels.begin(), _pixels.end(), false);
       }
 
       ImGui::EndGroup();
+
+      if (ImGui::Button("Shift Left")) {
+         bitmap_shift(ShiftDirection::Left);
+      }
+      ImGui::SameLine();
+      if (ImGui::Button("Shift Right")) {
+         bitmap_shift(ShiftDirection::Right);
+      }
+      ImGui::SameLine();
+      if (ImGui::Button("Shift Up")) {
+         bitmap_shift(ShiftDirection::Up);
+      }
+      ImGui::SameLine();
+      if (ImGui::Button("Shift Down")) {
+         bitmap_shift(ShiftDirection::Down);
+      }
+      ImGui::SameLine();
+      if (ImGui::Button("Invert")) {
+         bitmap_invert();
+      }
    }
 
    // clamp to hack screen size
-   pixel_height = std::clamp<decltype(pixel_height)>(pixel_height, 1, max_height);
-   pixel_width = std::clamp<decltype(pixel_width)>(pixel_width, 1, max_width);
+   _pixels_height = std::clamp<decltype(_pixels_height)>(_pixels_height, 1, max_height);
+   _pixels_width = std::clamp<decltype(_pixels_width)>(_pixels_width, 1, max_width);
 
+   ImGui::BeginChild("Bitmap Grid");
    ImGui::PushStyleVar(ImGuiStyleVar_CellPadding, ImVec2(0, 0));
    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
    ImGui::PushStyleVar(ImGuiStyleVar_ItemInnerSpacing, ImVec2(0, 0));
-   ImGui::BeginChild("Bitmap Grid");
-   if (ImGui::BeginTable("Bitmap", pixel_width,
+
+   // center drawing grid
+   const auto table_width = _pixels_width * button_size;
+   auto window_width = ImGui::GetWindowSize().x;
+   ImGui::SetCursorPosX((window_width - table_width) / 2);
+
+   if (ImGui::BeginTable("Bitmap", _pixels_width,
            ImGuiTableFlags_SizingFixedSame | ImGuiTableFlags_NoHostExtendX
                | ImGuiTableFlags_Borders)) {
       ImGuiListClipper clipper;
-      clipper.Begin(pixel_height);
+      clipper.Begin(_pixels_height);
       while (clipper.Step()) {
          for (int row = clipper.DisplayStart; row < clipper.DisplayEnd; row++) {
             ImGui::TableNextRow();
-            for (int col = 0; col < pixel_width; col++) {
+            for (int col = 0; col < _pixels_width; col++) {
                ImGui::TableNextColumn();
-               ImGui::PushID(row * pixel_width + col);
+               ImGui::PushID(row * _pixels_width + col);
 
-               auto &curr_pixel = pixels.at(row * pixel_width + col);
+               auto &curr_pixel = bitmap_at(col, row);
                bool should_pop_color = false;
                if (curr_pixel) {
                   should_pop_color = true;
@@ -71,7 +169,7 @@ void ViewCtx::show() {
                   ImGui::PushStyleColor(ImGuiCol_Button, Color::RED);
                }
 
-               ImGui::Button("##bitmap-pixel", ImVec2(15, 15));
+               ImGui::Button("##bitmap-pixel", ImVec2(button_size, button_size));
 
                static bool first_click_state = false;
                if (ImGui::IsItemHovered(ImGuiHoveredFlags_AllowWhenBlockedByActiveItem)) {
@@ -93,10 +191,8 @@ void ViewCtx::show() {
       }
       ImGui::EndTable();
    }
-   ImGui::EndChild();
    ImGui::PopStyleVar(3);
+   ImGui::EndChild();
 }
-
-std::string_view ViewCtx::view_name() const { return "Bitmap Editor"; }
 
 }
--- src/gui/bitmap.hpp
+++ src/gui/bitmap.hpp
@@ -2,14 +2,27 @@
 #define N2T_GUI_BITMAP_HPP
 
 #include "gui.hpp"
+#include <array>
 
 namespace gui::bitmap {
 
+constexpr std::uint16_t default_height = 16, default_width = 16;
+constexpr std::uint16_t max_height = 256, max_width = 512;
+
+enum class ShiftDirection { Left, Right, Up, Down };
+
 class ViewCtx final : public gui::BaseView {
    gui::Context *_ctx = nullptr;
+   std::uint16_t _pixels_height = default_height;
+   std::uint16_t _pixels_width = default_width;
+   std::array<bool, max_width * max_height> _pixels { };
 
    public:
    ViewCtx(gui::Context *ctx);
+
+   void bitmap_invert();
+   void bitmap_shift(ShiftDirection);
+   bool &bitmap_at(std::uint16_t x, std::uint16_t y);
    void show() override;
    std::string_view view_name() const override;
 };
